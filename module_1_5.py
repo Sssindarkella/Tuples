763ca3629517e8712c04adfa81d5a1caa7dbf2f2
Sindarkella <sssindarkella@gmail.com>
2024-10-05 22:27:39 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = (14,1.2,'Anna',True) # значения элементов кортежа нельзя изменить, потому что он не изменяем в отличие от списка
+print(immutable_var)
+mutable_list = ([10,'Python'],[1.2,'Sport'],False, 5.3)
+print(mutable_list)
+mutable_list [1][1]=89
+print(mutable_list)
